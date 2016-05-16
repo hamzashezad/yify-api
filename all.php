60cab36ccf625e3b6a3f9010aec88b82ae2f80b2
ArtyumX <didifsx@hotmail.fr>
2016-05-16 12:42:00 +0000

    Updated api link

--- all.php
+++ all.php
@@ -2,7 +2,7 @@
 
 $c = curl_init();
 
-curl_setopt($c, CURLOPT_URL, "https://yts.to/api/v2/list_movies.json");
+curl_setopt($c, CURLOPT_URL, "https://www.yifytorrent.xyz/api/v2/list_movies.json");
 
 curl_setopt($c,  CURLOPT_RETURNTRANSFER, 1);
 
